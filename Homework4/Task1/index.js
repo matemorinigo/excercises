0fad207b321f473a9c09e8234287621bdef8129e
Mateo <matemorinigo@gmail.com>
2024-04-20 21:25:23 -0300

    Homework4 task1 v1

---
+++ Homework4/Task1/index.js
@@ -0,0 +1,45 @@
+let person = {
+    _firstName: "John",
+    _lastName: "Doe",
+    _age: 30,
+    _email: "john.doe@example.com",
+
+    updateInfo(info){
+        if(info.firstName){
+            Object.defineProperty(this, '_firstName', {value: info.firstName});
+        }
+        if(info.lastName){
+            Object.defineProperty(this, '_lastName', {value: info.lastName});
+        }
+        if(info.age){
+            Object.defineProperty(this, '_age', {value: info.age});
+        }
+        if(info.email){
+            Object.defineProperty(this, '_email', {value: info.email});
+        }
+    },
+
+}
+
+Object.defineProperties(person, {
+    firstName:{
+        writable: false,
+        configurable: true
+    },
+    lastName:{
+        writable: false,
+        configurable:true
+    },
+    age:{
+        writable: false,
+        configurable: true
+    },
+    email:{
+        writable: false,
+        configurable: true
+    }
+});
+
+Object.defineProperty(person, '_address', {value: {}, enumerable: false, configurable: false});
+
+module.exports = {person};
