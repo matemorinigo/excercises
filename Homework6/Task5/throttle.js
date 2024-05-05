5366760efe2c8c5037696396a659103b50af32aa
Mateo <matemorinigo@gmail.com>
2024-05-05 10:20:24 -0300

    throttle added

---
+++ Homework6/Task5/throttle.js
@@ -0,0 +1,22 @@
+function throttle (func, interval){
+    let lastExec = -1;
+
+    return function(){
+
+        if(lastExec === -1 || (performance.now() - lastExec)>interval){
+            func();
+            lastExec = performance.now();
+        }
+
+    }
+}
+
+
+function onScroll(event) {
+    // Handle scroll event
+    console.log("Scroll event:", event);
+}
+
+const throttledScrollHandler = throttle(onScroll, 1000);
+
+window.addEventListener("scroll", throttledScrollHandler);
